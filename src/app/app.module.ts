2fcf007436d3412b0dcd1a8b061f1b4ed2370443
Mohamed Romdhani <mromdhani7@gmail.com>
2018-12-26 15:12:07 +0100

    Fin du formulaire ReactiveForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {HttpClientModule} from '@angular/common/http';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,8 +13,6 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ComptesListComponent } from './components/comptes-list/comptes-list.component';
 import { ComptesFilterPipe } from './shared/pipes/comptes-filter.pipe';
-import { ComptesService } from './sevices/comptes.service';
-import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ComptesDetailsComponent } from './components/comptes-details/comptes-details.component';
@@ -38,6 +36,7 @@ import { ComptesAddComponent } from './components/comptes-add/comptes-add.compon
     AppRoutingModule,
     NgbModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
   ],
   providers: [],
--- src/app/components/comptes-add/comptes-add.component.ts
+++ src/app/components/comptes-add/comptes-add.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ICompte } from 'src/app/domain/icompte';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { ComptesService } from 'src/app/sevices/comptes.service';
+import { Router } from '@angular/router';
+import { RESOURCE_CACHE_PROVIDER } from '@angular/platform-browser-dynamic';
 
 @Component({
   selector: 'app-comptes-add',
@@ -10,13 +14,27 @@ export class ComptesAddComponent implements OnInit {
 
   compte: ICompte = {numero: '', proprietaire: '', solde: 0};
 
-  constructor() { }
+   form: FormGroup ;
+
+  constructor(private _formBuilder: FormBuilder,
+              private _service: ComptesService,
+              private _router: Router
+    ) { }
 
   ngOnInit() {
+     this.form = this._formBuilder.group({
+        numero: new FormControl('', Validators.required),
+        proprietaire: new FormControl('', Validators.required),
+        solde: new FormControl('', [Validators.required, Validators.pattern('[+-]?[0-9]+')])
+      });
   }
 
-  ouvrirCompte() {
-
+  ouvrirCompte(c: ICompte) {
+      console.log(' ********   Recu l\'objet : ' + JSON.stringify(c));
+      this._service.addCompte(c).subscribe(
+        resp => this._router.navigateByUrl('/list'),
+        err => console.log(`ATTENTION, Il ya eu l\'erreur ${err}`)
+      );
   }
 
 }
